37c5975d008c75cff3d832aad2a7b5b57e0dc26f
OmarSaleh506 <amory2000338@gmail.com>
2022-06-26 23:43:01 +0300

    done HW08

--- Homeworksapp/src/routes/homeworks.ts
+++ Homeworksapp/src/routes/homeworks.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from 'fastify';
 export default async function (server: FastifyInstance) {
 	server.get('/', async (request, reply) => {
 		// add your code here
+		reply.send({'mes': 'hello omar'})
 	});
 
 	server.get('/Homework', async (request, reply) => {
 		// add your code here
+		reply.send({'mes': 'omar finish HM'})
 	});
 }
